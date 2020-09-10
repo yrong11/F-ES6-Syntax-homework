0a62ef3110319787b8d9cd76fd0fcc9bee32b844
yurong <rong.yu@thoughtworks.com>
2020-09-10 21:02:05 +0800

    [yurong] feat: done practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+  const {data, column} = input
+  const result = []
+  for(let info of data){
+    let tmp = {}
+    column.forEach((value, index) => {
+      tmp[value.name] = info[index]
+    })
+    result.push(tmp)
+
+  }
+  return result
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (item, section) => {
+  const result = item
+  for(let i = 0; i < section.length; i++){ 
+    result.splice(section[i].index+i, 0, section[i].content)
+  }
+  return result
+}
 export { inject };
