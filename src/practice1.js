483dbd1bfba1696c87b4237b6683d8b5ee08318e
kevindongzg <dongkevin94@gmail.com>
2020-09-22 20:06:28 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,5 +1,8 @@
+// TODO 可以思考一下可否用reduce来做
 const parseData = (input) => {
+  // TODO 解构可以优化到函数接受参数中
   const {data, column} = input
+  // TODO 使用map做数组与数组的转化，不用声明新数组
   const result = []
   for(let info of data){
     let tmp = {}
--- src/practice2.js
+++ src/practice2.js
@@ -1,5 +1,9 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
+// TODO 从先排序sections 和 反向插入 方面思考如何改进
 const inject = (item, section) => {
+  // TODO 当前场景下重新声明result没有太大必要
   const result = item
+  // TODO 尝试使用 for循环 以外的遍历方式
   for(let i = 0; i < section.length; i++){ 
     result.splice(section[i].index+i, 0, section[i].content)
   }
